06348ff3736e6303053f4765a246093efd10bd67
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-03-20 17:44:20 -0400

    Add issues.

--- models.py
+++ models.py
@@ -3,7 +3,22 @@ from opencivicdata.models import *
 
 import uuid, shortuuid
 
-class LobbyingRegistration(Event):
+def swap_type(obj, new_type):
+    o = new_type()
+    o.__dict__ = obj.__dict__
+    return o
+
+class ShortUUIDMixin(object):
+    # short uuid stuff
+    @property
+    def short_uuid(self):
+        return shortuuid.encode(uuid.UUID(self.id.split("/")[-1]))
+
+    @classmethod
+    def filter_by_short_uuid(cls, short_uuid):
+        return cls.objects.filter(id='ocd-%s/%s' % (cls._meta.get_field_by_name('id')[0].ocd_type, str(shortuuid.decode(short_uuid))))
+
+class LobbyingRegistration(Event, ShortUUIDMixin):
     class Meta:
         proxy = True
         app_label = 'opencivicdata'
@@ -12,25 +27,49 @@ class LobbyingRegistration(Event):
 
     @property
     def clients(self):
-        return [participant.organization for participant in self.participants.all() if participant.note == "client"]
+        return [swap_type(participant.organization, Client) for participant in self.participants.all() if participant.note == "client"]
 
     @property
     def registrants(self):
-        return [participant.organization for participant in self.participants.all() if participant.note == "registrant"]
+        return [swap_type(participant.organization, Registrant) for participant in self.participants.all() if participant.note == "registrant"]
 
     @property
     def lobbyists(self):
-        return [participant.person for participant in self.participants.all() if participant.note == "lobbyist"]
+        return [swap_type(participant.person, Lobbyist) for participant in self.participants.all() if participant.note == "lobbyist"]
 
     @property
     def issues(self):
-        return list(itertools.chain.from_iterable((item.subjects for item in self.agenda.all())))
+        return list(itertools.chain.from_iterable(([issues_by_code[subject] for subject in item.subjects] for item in self.agenda.all())))
 
-    # short uuid stuff
-    @property
-    def short_uuid(self):
-        return shortuuid.encode(uuid.UUID(self.id.split("/")[-1]))
+class Lobbyist(Person, ShortUUIDMixin):
+    class Meta:
+        proxy = True
+        app_label = 'opencivicdata'
 
-    @classmethod
-    def filter_by_short_uuid(cls, short_uuid):
-        return cls.objects.filter(id='ocd-event/%s' % str(shortuuid.decode(short_uuid)))
+    objects = Person.objects.filter(eventparticipant__note="lobbyist").as_manager()
+
+class Registrant(Organization, ShortUUIDMixin):
+    class Meta:
+        proxy = True
+        app_label = 'opencivicdata'
+
+    objects = Organization.objects.filter(eventparticipant__note="registrant").as_manager()
+
+class Client(Organization, ShortUUIDMixin):
+    class Meta:
+        proxy = True
+        app_label = 'opencivicdata'
+
+    objects = Organization.objects.filter(eventparticipant__note="client").as_manager()
+
+## Issues -- not really models, but same idea
+from sopr_lobbying_reference import GENERAL_ISSUE_CODES
+from django.utils.text import slugify
+
+issues_by_code = {}
+issues_by_slug = {}
+
+for issue in GENERAL_ISSUE_CODES:
+    issue['slug'] = slugify(unicode(issue['description']))
+    issues_by_code[issue['issue_code']] = issue
+    issues_by_slug[issue['slug']] = issue
---
+++ sopr_lobbying_reference.py
@@ -0,0 +1,616 @@
+FILING_TYPES = [
+    {
+        "action": "registration",
+        "code": "1",
+        "name": "REGISTRATION"
+    },
+    {
+        "action": "registration_amendment",
+        "code": "2",
+        "name": "REGISTRATION AMENDMENT"
+    },
+    {
+        "action": "report",
+        "code": "3",
+        "name": "MID-YEAR REPORT"
+    },
+    {
+        "action": "report",
+        "code": "4",
+        "name": "MID-YEAR (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "5",
+        "name": "MID-YEAR AMENDMENT"
+    },
+    {
+        "action": "termination",
+        "code": "6",
+        "name": "MID-YEAR TERMINATION"
+    },
+    {
+        "action": "termination_letter",
+        "code": "7",
+        "name": "MID-YEAR TERMINATION LETTER"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "8",
+        "name": "MID-YEAR TERMINATION AMENDMENT"
+    },
+    {
+        "action": "report",
+        "code": "9",
+        "name": "YEAR-END REPORT"
+    },
+    {
+        "action": "report",
+        "code": "10",
+        "name": "YEAR-END (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "11",
+        "name": "YEAR-END AMENDMENT"
+    },
+    {
+        "action": "termination",
+        "code": "12",
+        "name": "YEAR-END TERMINATION"
+    },
+    {
+        "action": "termination_letter",
+        "code": "13",
+        "name": "YEAR-END TERMINATION LETTER"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "14",
+        "name": "YEAR-END TERMINATION AMENDMENT"
+    },
+    {
+        "action": "termination",
+        "code": "15",
+        "name": "YEAR-END TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "termination",
+        "code": "16",
+        "name": "MID-YEAR TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "misc_termination",
+        "code": "17",
+        "name": "MISC TERM"
+    },
+    {
+        "action": "misc_document",
+        "code": "18",
+        "name": "MISC. DOC"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "19",
+        "name": "MID-YEAR TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "20",
+        "name": "MID-YEAR AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "21",
+        "name": "YEAR-END AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "22",
+        "name": "YEAR-END TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "misc_update",
+        "code": "29",
+        "name": "UPDATE PAGE IN A REPORT"
+    },
+    {
+        "action": "report",
+        "code": "51",
+        "name": "FIRST QUARTER REPORT"
+    },
+    {
+        "action": "report",
+        "code": "52",
+        "name": "FIRST QUARTER (NO ACTIVITY)"
+    },
+    {
+        "action": "termination",
+        "code": "53",
+        "name": "FIRST QUARTER TERMINATION"
+    },
+    {
+        "action": "termination",
+        "code": "54",
+        "name": "FIRST QUARTER TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "55",
+        "name": "FIRST QUARTER AMENDMENT"
+    },
+    {
+        "action": "report_amendment",
+        "code": "56",
+        "name": "FIRST QUARTER AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "57",
+        "name": "FIRST QUARTER TERMINATION AMENDMENT"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "58",
+        "name": "FIRST QUARTER TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_letter",
+        "code": "59",
+        "name": "FIRST QUARTER TERMINATION LETTER"
+    },
+    {
+        "action": "report",
+        "code": "60",
+        "name": "SECOND QUARTER REPORT"
+    },
+    {
+        "action": "report",
+        "code": "61",
+        "name": "SECOND QUARTER (NO ACTIVITY)"
+    },
+    {
+        "action": "termination",
+        "code": "62",
+        "name": "SECOND QUARTER TERMINATION"
+    },
+    {
+        "action": "termination",
+        "code": "63",
+        "name": "SECOND QUARTER TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "64",
+        "name": "SECOND QUARTER AMENDMENT"
+    },
+    {
+        "action": "report_amendment",
+        "code": "65",
+        "name": "SECOND QUARTER AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "66",
+        "name": "SECOND QUARTER TERMINATION AMENDMENT"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "67",
+        "name": "SECOND QUARTER TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_letter",
+        "code": "68",
+        "name": "SECOND QUARTER TERMINATION LETTER"
+    },
+    {
+        "action": "report",
+        "code": "69",
+        "name": "THIRD QUARTER REPORT"
+    },
+    {
+        "action": "report",
+        "code": "70",
+        "name": "THIRD QUARTER (NO ACTIVITY)"
+    },
+    {
+        "action": "termination",
+        "code": "71",
+        "name": "THIRD QUARTER TERMINATION"
+    },
+    {
+        "action": "termination",
+        "code": "72",
+        "name": "THIRD QUARTER TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "73",
+        "name": "THIRD QUARTER AMENDMENT"
+    },
+    {
+        "action": "report_amendment",
+        "code": "74",
+        "name": "THIRD QUARTER AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "75",
+        "name": "THIRD QUARTER TERMINATION AMENDMENT"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "76",
+        "name": "THIRD QUARTER TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_letter",
+        "code": "77",
+        "name": "THIRD QUARTER TERMINATION LETTER"
+    },
+    {
+        "action": "report",
+        "code": "78",
+        "name": "FOURTH QUARTER REPORT"
+    },
+    {
+        "action": "report",
+        "code": "79",
+        "name": "FOURTH QUARTER (NO ACTIVITY)"
+    },
+    {
+        "action": "termination",
+        "code": "80",
+        "name": "FOURTH QUARTER TERMINATION"
+    },
+    {
+        "action": "termination",
+        "code": "81",
+        "name": "FOURTH QUARTER TERMINATION (NO ACTIVITY)"
+    },
+    {
+        "action": "report_amendment",
+        "code": "82",
+        "name": "FOURTH QUARTER AMENDMENT"
+    },
+    {
+        "action": "report_amendment",
+        "code": "83",
+        "name": "FOURTH QUARTER AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "84",
+        "name": "FOURTH QUARTER TERMINATION AMENDMENT"
+    },
+    {
+        "action": "termination_amendment",
+        "code": "85",
+        "name": "FOURTH QUARTER TERMINATION AMENDMENT (NO ACTIVITY)"
+    },
+    {
+        "action": "termination_letter",
+        "code": "86",
+        "name": "FOURTH QUARTER TERMINATION LETTER"
+    }
+]
+
+GENERAL_ISSUE_CODES = [
+    {
+        "issue_code": "ACC",
+        "description": "Accounting"
+    },
+    {
+        "issue_code": "CPI",
+        "description": "Computer Industry"
+    },
+    {
+        "issue_code": "AER",
+        "description": "Aerospace"
+    },
+    {
+        "issue_code": "REL",
+        "description": "Religion"
+    },
+    {
+        "issue_code": "MIA",
+        "description": "Media (Information/Publishing)"
+    },
+    {
+        "issue_code": "DOC",
+        "description": "District of Columbia"
+    },
+    {
+        "issue_code": "CAW",
+        "description": "Clean Air & Water (Quality)"
+    },
+    {
+        "issue_code": "CPT",
+        "description": "Copyright/Patent/Trademark"
+    },
+    {
+        "issue_code": "ANI",
+        "description": "Animals"
+    },
+    {
+        "issue_code": "TOB",
+        "description": "Tobacco"
+    },
+    {
+        "issue_code": "FUE",
+        "description": "Fuel/Gas/Oil"
+    },
+    {
+        "issue_code": "TOU",
+        "description": "Travel/Tourism"
+    },
+    {
+        "issue_code": "CIV",
+        "description": "Civil Rights/Civil Liberties"
+    },
+    {
+        "issue_code": "NAT",
+        "description": "Natural Resources"
+    },
+    {
+        "issue_code": "BAN",
+        "description": "Banking"
+    },
+    {
+        "issue_code": "BEV",
+        "description": "Beverage Industry"
+    },
+    {
+        "issue_code": "AGR",
+        "description": "Agriculture"
+    },
+    {
+        "issue_code": "DEF",
+        "description": "Defense"
+    },
+    {
+        "issue_code": "CON",
+        "description": "Constitution"
+    },
+    {
+        "issue_code": "MMM",
+        "description": "Medicare/Medicaid"
+    },
+    {
+        "issue_code": "GOV",
+        "description": "Government Issues"
+    },
+    {
+        "issue_code": "SCI",
+        "description": "Science/Technology"
+    },
+    {
+        "issue_code": "URB",
+        "description": "Urban Development/Municipalities"
+    },
+    {
+        "issue_code": "TAR",
+        "description": "Miscellaneous Tariff Bills"
+    },
+    {
+        "issue_code": "COM",
+        "description": "Communications/Broadcasting/Radio/TV"
+    },
+    {
+        "issue_code": "TAX",
+        "description": "Taxation/Internal Revenue Code"
+    },
+    {
+        "issue_code": "TEC",
+        "description": "Telecommunications"
+    },
+    {
+        "issue_code": "ROD",
+        "description": "Roads/Highway"
+    },
+    {
+        "issue_code": "POS",
+        "description": "Postal"
+    },
+    {
+        "issue_code": "RET",
+        "description": "Retirement"
+    },
+    {
+        "issue_code": "TOR",
+        "description": "Torts"
+    },
+    {
+        "issue_code": "GAM",
+        "description": "Gaming/Gambling/Casino"
+    },
+    {
+        "issue_code": "SMB",
+        "description": "Small Business"
+    },
+    {
+        "issue_code": "FAM",
+        "description": "Family Issues/Abortion/Adoption"
+    },
+    {
+        "issue_code": "WAS",
+        "description": "Waste (hazardous/solid/interstate/nuclear)"
+    },
+    {
+        "issue_code": "UTI",
+        "description": "Utilities"
+    },
+    {
+        "issue_code": "DIS",
+        "description": "Disaster Planning/Emergencies"
+    },
+    {
+        "issue_code": "WEL",
+        "description": "Welfare"
+    },
+    {
+        "issue_code": "RRR",
+        "description": "Railroads"
+    },
+    {
+        "issue_code": "BUD",
+        "description": "Budget/Appropriations"
+    },
+    {
+        "issue_code": "MON",
+        "description": "Minting/Money/Gold Standard"
+    },
+    {
+        "issue_code": "ADV",
+        "description": "Advertising"
+    },
+    {
+        "issue_code": "VET",
+        "description": "Veterans"
+    },
+    {
+        "issue_code": "HOM",
+        "description": "Homeland Security"
+    },
+    {
+        "issue_code": "TRU",
+        "description": "Trucking/Shipping"
+    },
+    {
+        "issue_code": "UNM",
+        "description": "Unemployment"
+    },
+    {
+        "issue_code": "FOR",
+        "description": "Foreign Relations"
+    },
+    {
+        "issue_code": "ENG",
+        "description": "Energy/Nuclear"
+    },
+    {
+        "issue_code": "FIR",
+        "description": "Firearms/Guns/Ammunition"
+    },
+    {
+        "issue_code": "EDU",
+        "description": "Education"
+    },
+    {
+        "issue_code": "IMM",
+        "description": "Immigration"
+    },
+    {
+        "issue_code": "CHM",
+        "description": "Chemicals/Chemical Industry"
+    },
+    {
+        "issue_code": "TRD",
+        "description": "Trade (Domestic & Foreign)"
+    },
+    {
+        "issue_code": "BNK",
+        "description": "Bankruptcy"
+    },
+    {
+        "issue_code": "HCR",
+        "description": "Health Issues"
+    },
+    {
+        "issue_code": "HOU",
+        "description": "Housing"
+    },
+    {
+        "issue_code": "AUT",
+        "description": "Automotive Industry"
+    },
+    {
+        "issue_code": "ENV",
+        "description": "Environmental/Superfund"
+    },
+    {
+        "issue_code": "RES",
+        "description": "Real Estate/Land Use/Conservation"
+    },
+    {
+        "issue_code": "FOO",
+        "description": "Food Industry (Safety, Labeling, etc.)"
+    },
+    {
+        "issue_code": "FIN",
+        "description": "Financial Institutions/Investments/Securities"
+    },
+    {
+        "issue_code": "CSP",
+        "description": "Consumer Issues/Safety/Protection"
+    },
+    {
+        "issue_code": "MED",
+        "description": "Medical/Disease Research/Clinical Labs"
+    },
+    {
+        "issue_code": "MAR",
+        "description": "Marine/Maritime/Boating/Fisheries"
+    },
+    {
+        "issue_code": "ART",
+        "description": "Arts/Entertainment"
+    },
+    {
+        "issue_code": "INT",
+        "description": "Intelligence and Surveillance"
+    },
+    {
+        "issue_code": "APP",
+        "description": "Apparel/Clothing Industry/Textiles"
+    },
+    {
+        "issue_code": "TRA",
+        "description": "Transportation"
+    },
+    {
+        "issue_code": "ALC",
+        "description": "Alcohol & Drug Abuse"
+    },
+    {
+        "issue_code": "INS",
+        "description": "Insurance"
+    },
+    {
+        "issue_code": "CDT",
+        "description": "Commodities (Big Ticket)"
+    },
+    {
+        "issue_code": "LBR",
+        "description": "Labor Issues/Antitrust/Workplace"
+    },
+    {
+        "issue_code": "AVI",
+        "description": "Aviation/Aircraft/Airlines"
+    },
+    {
+        "issue_code": "ECN",
+        "description": "Economics/Economic Development"
+    },
+    {
+        "issue_code": "IND",
+        "description": "Indian/Native American Affairs"
+    },
+    {
+        "issue_code": "SPO",
+        "description": "Sports/Athletics"
+    },
+    {
+        "issue_code": "LAW",
+        "description": "Law Enforcement/Crime/Criminal Justice"
+    },
+    {
+        "issue_code": "PHA",
+        "description": "Pharmacy"
+    },
+    {
+        "issue_code": "MAN",
+        "description": "Manufacturing"
+    }
+]
