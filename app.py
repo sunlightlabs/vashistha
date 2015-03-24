13e4ed913e2417519edd62a2d46e46eba7246ae1
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-03-24 13:33:27 -0400

    Add dryrub inheritance for most of the rest of the templates.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ try:
 except:
     local_settings = None
 
-import djmicro, os
+import djmicro, os, re
 djmicro.configure({
     'INSTALLED_APPS': ('opencivicdata', 'django.contrib.staticfiles', 'dryrub'),
     'STATIC_URL': '/static/',
@@ -110,7 +110,8 @@ class ParticipantListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context_data = super(ParticipantListView, self).get_context_data(*args, **kwargs)
-        context_data['participants_by_alpha'] = [(letter, list(group)) for letter, group in itertools.groupby(self.model.objects.all().order_by('name'), key=lambda org: org.name[0].upper())]
+        nonletters = re.compile('[^A-Z0-9]')
+        context_data['participants_by_alpha'] = [(letter, list(group)) for letter, group in itertools.groupby(self.model.objects.all().order_by('name'), key=lambda org: nonletters.sub('', org.name.upper())[0])]
         context_data['participant_type'] = self.participant_type
         return context_data
 
