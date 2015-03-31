eccb4c98a6358bd5c4db891ff8440820fdebb485
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-03-31 14:50:16 -0400

    Get ready for elasticsearch on Heroku

--- heroku_settings.py
+++ heroku_settings.py
@@ -1,14 +1,8 @@
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-import os, dj_database_url
+import os, dj_database_url, dj_haystack_url
 
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack_redis.RedisEngine',
-        'PATH': os.environ['REDIS_URL'],
-    },
-}
-
+HAYSTACK_CONNECTIONS = {'default': dj_haystack_url.config()}
 DATABASES = {'default': dj_database_url.config()}
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'heroku_static')
