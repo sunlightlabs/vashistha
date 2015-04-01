bfa8545d37c3181a5f8318e72264a9e7ae3fdc74
Andrew Pendleton <abpend@gmail.com>
2015-03-31 23:54:05 -0400

    Get ready for redis caching in production

--- heroku_settings.py
+++ heroku_settings.py
@@ -1,8 +1,10 @@
-DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-import os, dj_database_url, dj_haystack_url
+import os, dj_database_url, dj_haystack_url, django_cache_url
+
+DEBUG = {'False': False, 'True': True}[os.environ.get('DEBUG', 'False')]
 
 HAYSTACK_CONNECTIONS = {'default': dj_haystack_url.config()}
 DATABASES = {'default': dj_database_url.config()}
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'heroku_static')
+CACHES = {'default': django_cache_url.config()}
